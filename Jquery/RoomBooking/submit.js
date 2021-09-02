35cbed4c0e3df4fab25d87e7d6dbb43ce569cdc4
pawar-shailesh <89083005+pawar-shailesh@users.noreply.github.com>
2021-09-02 11:51:46 +0530

    Add files via upload

---
+++ Jquery/RoomBooking/submit.js
@@ -0,0 +1,53 @@
+
+        // $(document).ready(function () {
+        //     // Get value on button click and show alert
+        //     $("#booking").click(function () {
+        //         var fromdate = $("#fromdate").val();
+        //         var todate = $("#todate").val();
+        //         var purpose=$("#purpose").val();
+        //         alert(fromdate+todate+purpose);
+
+        //         var show = $("#show").val();
+        //         var newRow = show.insertRow(("#show").length);
+        //         var c1 = newRow.insertCell(0);
+        //         var c2 = newRow.insertCell(1);
+        //         var c3 = newRow.insertCell(2);
+        //         var c4 = newRow.insertCell(3);
+
+        //         c1.innerHTML = fromdate;
+        //         c2.innerHTML = todate;
+        //         c3.innerHTML = purpose;
+        //         c4.innerHTML = numberOfPeoples
+        //         row++;
+        //     });
+        // });
+
+// function roomBooking() {
+//     // console.log("Room Booking Sucessfull...");
+//     var fromdate = document.getElementById("fromdate").value;
+//     var todate = document.getElementById("todate").value;
+//     var purpose = document.getElementById("purpose").value;
+//     var numberOfPeoples=document.getElementById("peoples").value;
+//     var id=document.getElementById("employeeid").value;
+
+//     if(!fromdate || !todate || !purpose ||!numberOfPeoples)
+//     {
+//         alert("Booking Sucessful...");
+//         return;
+//     }
+   
+
+//     var show=document.getElementById("show");
+//     var newRow=show.insertRow(show.length);
+//     var c1=newRow.insertCell(0);
+//     var c2=newRow.insertCell(1);
+//     var c3=newRow.insertCell(2);
+//     var c4=newRow.insertCell(3);
+
+//     c1.innerHTML=fromdate;
+//     c2.innerHTML=todate;
+//     c3.innerHTML=purpose;
+//     c4.innerHTML=numberOfPeoples
+//     row++;
+
+// }
